44e7f61bf0e0044effcfb61e17ee1670861cd30b
Chris Wolf <chriswolfdesign@gmail.com>
2024-04-16 18:31:29 -0400

    Application now prints current item in highlighted color

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use reqwest::Error;
 use serde::Deserialize;
-
-fn clear_terminal() {
-    std::process::Command::new("clear").status().unwrap();
-}
+use colored::Colorize;
 
 #[derive(Deserialize, Debug)]
 struct Todo {
@@ -12,18 +9,38 @@ struct Todo {
     completed: bool,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Error>{
-    clear_terminal();
+fn clear_terminal() {
+    std::process::Command::new("clear").status().unwrap();
+}
 
+async fn get_todos() -> Result<Vec<Todo>, Error> {
     let request_url = "http://127.0.0.1:8000/todos";
-    println!("{}", request_url);
 
     let response = reqwest::get(request_url).await?;
-    let users: Vec<Todo> = response.json().await?;
+    let todos = response.json().await?;
 
-    dbg!(users);
+    Ok(todos)
+}
 
-    return Ok(())
+fn print_todos(todos: Vec<Todo>, curr: usize) {
+    for (index, todo) in todos.iter().enumerate() {
+        if index == curr {
+            println!("{}", todo.text.black().on_white());
+        } else {
+            println!("{}", todo.text);
+        }
+    }
+}
 
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    clear_terminal();
+
+    let todos = get_todos().await?;
+
+    let curr = 0;
+
+    print_todos(todos, curr);
+
+    return Ok(());
 }
